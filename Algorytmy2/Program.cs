4a6df3843fbda9b1df42ab5dc692a6314986d624
Radoslaw Stefanski <cabreraytpl@gmail.com>
2024-04-19 10:23:40 +0200

    init

---
+++ Algorytmy2/Program.cs
@@ -0,0 +1,9 @@
+﻿using Algorytmy2;
+
+Sortowanie sort = new([5, 7, 2, 2, 1, 4, 5, 6]);
+Console.WriteLine(sort.Sort());
+int[] t = sort.FindLowestAndExchange();
+foreach (var item in t)
+{
+    Console.Write($"{item}, ");
+}
---
+++ Algorytmy2/Sortowanie.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorytmy2
+{
+    public class Sortowanie(int[] cyfry)
+    {
+        public int[] _cyfry = cyfry;
+
+        /**************************************
+        * nazwa funkcji:       Sort()
+        * parametry wejściowe: Bierze wartosci z int[] _cyfry.
+        * wartość zwracana:    int, najmniejsza liczba z arrayu
+        * autor:               Radosław Stefański 3D
+        *************************************/
+        public int Sort()
+        {
+            int currentLowest = int.MaxValue;
+
+            foreach (int cyfra in cyfry)
+            {
+                if (cyfra < currentLowest)
+                    currentLowest = cyfra;
+            }
+
+            return currentLowest;
+        }
+
+        /**************************************
+         * nazwa funkcji:       FindLowestAndExchange()
+         * parametry wejściowe: Bierze wartosci z int[] _cyfry.
+         * wartość zwracana:    int[]
+         * autor:               Radosław Stefański 3D
+        *************************************/
+        public int[] FindLowestAndExchange()
+        {
+            int[] _cyfryClone = (int[])_cyfry.Clone();
+
+            for (var i = 0; i < _cyfry.Length; i++)
+            {
+                if (_cyfryClone[i] == Sort())
+                    _cyfryClone[i] = i;
+            }
+
+            return _cyfryClone;
+        }
+    }
+}
